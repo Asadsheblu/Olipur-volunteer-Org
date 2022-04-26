17b3baacc8593e1fd2bbf3838ad1c74eb15d8da1
Asadsheblu <shebluasad@gmail.com>
2022-04-27 00:26:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require("express")
+const app=express()
+
+require('dotenv').config()
+const cors=require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express')
+app.use(cors())
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send("Hello Olipur  backend sever")
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7auxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const Volunteercollection=client.db("Olipur").collection("volunteer")
+       app.get('/volunteer',async(req,res)=>{
+        const query={}
+        const result=Volunteercollection.find(query)
+        const review=await result.toArray()
+        res.send(review)
+       })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir)
+app.listen(port,()=>{
+    console.log(`Running server ${port}`);
+})
